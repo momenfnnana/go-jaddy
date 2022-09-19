7fe46a0b05db4b3a8d5d2a406a852c77de7f8b79
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-19 22:39:57 +0300

    move some logic of navigation

--- App.tsx
+++ App.tsx
@@ -1,23 +1,7 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {AuthStack, HomeStack} from 'navigators';
-import {AuthRoutes, HomeRoutes} from 'navigators/RoutesTypes';
+import AppIndex from './src/App';
 import 'i18n';
 
-type MainNavigator = HomeRoutes & AuthRoutes;
-const Stack = createNativeStackNavigator<MainNavigator>();
-
 export default function App() {
-  const token = true;
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={token ? 'Home' : 'Login'}
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen component={HomeStack} name="Home" />
-        <Stack.Screen component={AuthStack} name="Login" />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppIndex />;
 }
---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {AuthStack, HomeStack} from 'navigators';
+import {AuthRoutes, HomeRoutes} from 'navigators/RoutesTypes';
+
+type MainNavigator = HomeRoutes & AuthRoutes;
+const Stack = createNativeStackNavigator<MainNavigator>();
+const App = () => {
+  const token = true;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={token ? 'Home' : 'Login'}
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen component={HomeStack} name="Home" />
+        <Stack.Screen component={AuthStack} name="Login" />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
