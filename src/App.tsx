378442128451ad9a42856bb50a1cc8332ce77634
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-20 18:07:30 +0300

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {AuthStack, HomeStack} from 'navigators';
-import {AuthRoutes, HomeRoutes} from 'navigators/RoutesTypes';
+import {MainNavigator} from 'navigators/RoutesTypes';
 
-type MainNavigator = HomeRoutes & AuthRoutes;
 const Stack = createNativeStackNavigator<MainNavigator>();
 const App = () => {
   const token = true;
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName={token ? 'Home' : 'Login'}
+        initialRouteName={token ? 'HomeFlow' : 'LoginFlow'}
         screenOptions={{headerShown: false}}>
-        <Stack.Screen component={HomeStack} name="Home" />
-        <Stack.Screen component={AuthStack} name="Login" />
+        <Stack.Screen component={HomeStack} name="HomeFlow" />
+        <Stack.Screen component={AuthStack} name="LoginFlow" />
       </Stack.Navigator>
     </NavigationContainer>
   );
