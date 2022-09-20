0de41130ff12eb5e7b7a94620730bf2f6468034e
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-20 21:15:28 +0300

    add new configurations for network state

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
-import * as React from 'react';
+import React from 'react';
+import {useNetInfo} from '@react-native-community/netinfo';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
 import AppIndex from './src/App';
 import 'i18n';
+import {NetworkErrorScreen} from 'screens';
 
 export default function App() {
+  const {isConnected} = useNetInfo();
+
+  if (!isConnected) {
+    return (
+      <SafeAreaProvider>
+        <NetworkErrorScreen />
+      </SafeAreaProvider>
+    );
+  }
   return <AppIndex />;
 }
