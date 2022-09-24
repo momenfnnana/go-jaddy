361d058c3a2dff1c33e96fd8731ea40534206545
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-24 21:25:18 +0300

    export screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,10 @@
-export {HomeScreen, ProfileScreen} from './main';
+export {
+  HomeScreen,
+  ProfileScreen,
+  ProductDetailsScreen,
+  CategoriesScreen,
+  StoresScreen,
+  CartScreen,
+} from './main';
 export {OnboardingScreen, LoginScreen, RegisterScreen} from './Onboading';
 export {default as NetworkErrorScreen} from './NetworkErrorScreen';
