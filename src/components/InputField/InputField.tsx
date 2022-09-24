d293eeb32b3158544787b4359238cff5a06c8541
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-25 01:08:11 +0300

    add new input field component

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,52 @@
+import React, {ReactNode} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TextInputProps,
+  useWindowDimensions,
+  ViewStyle,
+} from 'react-native';
+import {useTranslation} from 'react-i18next';
+import {colors, spacing} from 'theme';
+
+interface IInputField extends TextInputProps {
+  rightIcon?: ReactNode;
+  containerStyle?: ViewStyle;
+}
+
+const InputField = ({rightIcon, containerStyle, ...rest}: IInputField) => {
+  const {t} = useTranslation();
+  const {width} = useWindowDimensions();
+  return (
+    <View
+      style={[
+        styles.containerStyle,
+        {justifyContent: rightIcon ? 'space-between' : 'center'},
+        containerStyle,
+      ]}>
+      <TextInput
+        placeholder={t('common.phone-numebr')}
+        style={[styles.textInput, {width: width * 0.65}]}
+        {...rest}
+      />
+      <View style={{width: width * 0.2}}>{rightIcon && rightIcon}</View>
+    </View>
+  );
+};
+
+export default InputField;
+const styles = StyleSheet.create({
+  textInput: {},
+  containerStyle: {
+    borderColor: colors.reloadColor,
+    borderWidth: 1,
+    alignSelf: 'center',
+    paddingVertical: spacing.large,
+    borderRadius: spacing.small,
+    paddingHorizontal: spacing.normal,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: colors.white,
+  },
+});
