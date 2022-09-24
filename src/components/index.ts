24704c311f5fa3b5e80d7bb870e2a0748fce0750
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-25 01:08:25 +0300

    export input field component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {default as Button} from './Button';
 export {default as Text} from './Text';
 export {default as ScreenContainer} from './ScreenContainer';
+export {default as InputField} from './InputField';
