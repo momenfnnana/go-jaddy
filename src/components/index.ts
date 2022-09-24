a0228c654482b930d95af2c354c1a2b87ed6cf9d
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-24 21:18:27 +0300

    export shared components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export {default as Button} from './Button';
 export {default as Text} from './Text';
 export {default as ScreenContainer} from './ScreenContainer';
+export {default as MainHeader} from './MainHeader';
+export {default as SearchInput} from './SearchInput';
+export {default as ProductCard} from './ProductCard';
+export {default as Loader} from './Loader';
