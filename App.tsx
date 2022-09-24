54644ff5daddc734b55db9187660ef51bf02d098
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-24 21:17:02 +0300

    wrap project with query provider for making api calls

--- App.tsx
+++ App.tsx
@@ -5,16 +5,24 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppIndex from './src/App';
 import 'i18n';
 import {NetworkErrorScreen} from 'screens';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 export default function App() {
   const {isConnected} = useNetInfo();
+  const queryClient = new QueryClient();
 
   if (!isConnected) {
     return (
-      <SafeAreaProvider>
-        <NetworkErrorScreen />
-      </SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <NetworkErrorScreen />
+        </SafeAreaProvider>
+      </QueryClientProvider>
     );
   }
-  return <AppIndex />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppIndex />
+    </QueryClientProvider>
+  );
 }
