61a474fdcf226b14220ef982322f2bf388cd56da
Momen Fnnana <mommen@hemma.sa>
2022-09-26 22:28:37 +0300

    add loader and disable for button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,23 +2,36 @@ import {View, ViewStyle, Pressable, StyleSheet} from 'react-native';
 import React from 'react';
 import {colors, spacing} from 'theme';
 import Text from 'components/Text';
+import Loader from 'components/Loader';
 
 interface buttomProps {
   style?: ViewStyle;
   title: string;
   onPress?: any;
   color?: string;
+  isLoading?: boolean;
 }
 
-const Button: React.FC<buttomProps> = ({style, title, onPress, color}) => {
+const Button: React.FC<buttomProps> = ({
+  style,
+  title,
+  onPress,
+  color,
+  isLoading,
+}) => {
   return (
-    <Pressable onPress={onPress} style={[styles.pressable, style]}>
+    <Pressable
+      onPress={onPress}
+      style={[styles.pressable, style]}
+      disabled={isLoading}>
       <Text
         center
         variant="largeBold"
         tx={title}
         color={color || colors.white}
+        style={styles.textStyle}
       />
+      {isLoading && <Loader size="small" />}
     </Pressable>
   );
 };
@@ -27,9 +40,14 @@ export default Button;
 
 const styles = StyleSheet.create({
   pressable: {
+    flexDirection: 'row',
     borderRadius: spacing.xxLarge + 2,
     backgroundColor: colors.blue,
     paddingHorizontal: spacing.large,
     paddingVertical: spacing.small + 2,
+    alignItems: 'center',
+  },
+  textStyle: {
+    flex: 1,
   },
 });
