66251e729475b5160d4cb7698a1263f68a5c71b8
Momen Fnnana <mommen@hemma.sa>
2022-09-26 22:28:02 +0300

    change reading accessToken logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {AuthStack, HomeStack} from 'navigators';
 import {MainNavigator} from 'navigators/RoutesTypes';
-import {StatusBar} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Loader} from 'components';
 
 const Stack = createNativeStackNavigator<MainNavigator>();
 const App = () => {
-  const token = false;
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    (async () => {
+      const accessToken = await AsyncStorage.getItem('accessToken');
+      accessToken ? setIsLoggedIn(true) : setIsLoggedIn(false);
+      setLoading(false);
+    })();
+  }, []);
+  if (loading) {
+    return <Loader style={styles.loader} />;
+  }
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" />
       <Stack.Navigator
-        initialRouteName={token ? 'HomeFlow' : 'AuthFlow'}
+        initialRouteName={isLoggedIn ? 'HomeFlow' : 'AuthFlow'}
         screenOptions={{headerShown: false}}>
         <Stack.Screen component={HomeStack} name="HomeFlow" />
         <Stack.Screen component={AuthStack} name="AuthFlow" />
@@ -22,3 +36,11 @@ const App = () => {
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
