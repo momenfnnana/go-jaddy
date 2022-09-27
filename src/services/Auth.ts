23e5fa6c6a5d5441792013f073c1fea842a65cb7
Momen Fnnana <mommen@hemma.sa>
2022-09-27 20:45:46 +0300

    add new request for reset password

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -4,3 +4,7 @@ import {BASE_URL} from 'utils/Axios';
 export const doLogin_service = (data: any) => {
   return axios.post(`${BASE_URL}/api/custom/account/login`, data);
 };
+
+export const reset_password = (data: any) => {
+  return axios.post(`${BASE_URL}/api/custom/account/ResetPassword`, data);
+};
