5a58cccac6f1bce19a7e8b0e378d1c507e427fba
Momen Fnnana <mommen@hemma.sa>
2022-09-28 01:02:32 +0300

    add logout button for profile page

--- src/screens/main/Profile/Profile.tsx
+++ src/screens/main/Profile/Profile.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useContext} from 'react';
+import {Pressable, View} from 'react-native';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {
   NativeStackNavigationProp,
@@ -8,20 +8,29 @@ import {
 import {BottomTabsRoutes} from 'navigators/RoutesTypes';
 import type {RouteProp} from '@react-navigation/native';
 import {Text} from 'components';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {UserContext} from 'context/UserContext';
 
 interface IProfileNavigation
   extends NativeStackNavigationProp<BottomTabsRoutes, 'Profile'>,
     RouteProp<BottomTabsRoutes, 'Profile'> {}
 
-interface IProfile extends NativeStackScreenProps<BottomTabsRoutes, 'Profile'> {}
+interface IProfile
+  extends NativeStackScreenProps<BottomTabsRoutes, 'Profile'> {}
 const Profile = (props: IProfile) => {
   const {navigate} = useNavigation<IProfileNavigation>();
   const {params} = useRoute<IProfileNavigation>();
   const {userId} = params;
-
+  const {setUserData} = useContext(UserContext);
+  const logoutHandler = () => {
+    AsyncStorage.clear();
+    setUserData({});
+  };
   return (
     <View>
-      <Text text="Profile" />
+      <Pressable onPress={logoutHandler}>
+        <Text text="Logout" />
+      </Pressable>
     </View>
   );
 };
