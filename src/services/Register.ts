a5cacaa223e6c76b4a053985f3d7656de907834e
Momen Fnnana <mommen@hemma.sa>
2022-09-28 01:05:42 +0300

    cleaning register request

--- src/services/Register.ts
+++ src/services/Register.ts
@@ -1,13 +1,10 @@
 import axios from 'axios';
 import {BASE_URL} from 'utils/Axios';
 
-export const createAccount = async(data: any) => {
-  console.log('data in axios: ', data);
-  let response;
-  try {
-    response = await axios.post(`${BASE_URL}/api/custom/account/register`, data);
-  } catch (errorReq) {
-    console.log({"fdfd":errorReq?.error});
-  }
-  return response;
+export const createAccount = async (data: any) => {
+  return await axios.post(`${BASE_URL}/api/custom/account/register`, data, {
+    headers: {
+      'Content-Type': 'multipart/form-data; ',
+    },
+  });
 };
