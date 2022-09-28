85def86bc0cb3dcf9daeb782fedae4ed3654ac86
Momen Fnnana <mommen@hemma.sa>
2022-09-29 01:29:57 +0300

    add new services for home stack

--- src/services/Home.ts
+++ src/services/Home.ts
@@ -1,12 +1,25 @@
 import axios from 'axios';
 import {BASE_URL} from 'utils/Axios';
 
-export const getStores = () => {
-  return axios(`${BASE_URL}/api/custom/stores/HomeStores`);
-};
+export const getStores = () =>
+  axios(`${BASE_URL}/api/custom/stores/HomeStores`);
 
-export const getProductDetails = (id: number) => {
-  return axios(
-    `${BASE_URL}/api/custom/products/ProductDetails?productId=${id}`,
+export const getSlider = () =>
+  axios(`${BASE_URL}/api/custom/sliders?codes=Home_S_1,Home_S_2`);
+
+export const getRecentAdded = () =>
+  axios(`${BASE_URL}/api/custom/products/RecentlyAddedProducts`);
+
+export const getProductDetails = (id: number) =>
+  axios(`${BASE_URL}/api/custom/products/ProductDetails?productId=${id}`);
+
+export const getAdvertisements = () =>
+  axios(
+    `${BASE_URL}/api/custom/advertisements?codes=Header_Navigation_Categories_One`,
   );
-};
+
+export const getBestSellers = () =>
+  axios(`${BASE_URL}/api/custom/products/BestSellers`);
+
+export const getSearchResults = (trem: string) =>
+  axios(`${BASE_URL}/api/custom/search/ByTerm?term=${trem}`);
