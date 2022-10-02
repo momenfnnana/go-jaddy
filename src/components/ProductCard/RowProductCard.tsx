5526ba7036b12355646fc36bfb043dc88c06485e
Momen Fnnana <mommen@hemma.sa>
2022-10-03 00:38:16 +0300

    add product row card component

--- src/components/ProductCard/RowProductCard.tsx
+++ src/components/ProductCard/RowProductCard.tsx
@@ -1,38 +1,52 @@
+import {DiscountIcon, FavoriteIcon} from 'assets/icons';
 import Text from 'components/Text';
 import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, ImageBackground} from 'react-native';
 import {IProductInterface} from 'screens/main/Home/types';
 import {colors, spacing} from 'theme';
 import {BASE_URL} from 'utils/Axios';
 
 const RowProductCard = (props: IProductInterface) => {
   const {
-    // title,
     Image: ImageResponse,
     Price,
     Badges,
-    AddedToCart,
     Name,
-    RatingSum,
-    // isHaveDiscount,
-    // discountValue,
-    // acttualPrice,
-    // prevPrice,
-    // currency,
-    // rate,
-    // productColors,
-    // isNews,
-    // isFav,
+    currency,
+    CategoryName,
+    SupportMultiWishlists,
+    WishlistEnabled,
   } = props;
-  console.log({props});
+  const DiscountBadge = Badges.find(item => item?.Style === 5);
+  const isNewBadge = Badges.find(item => item?.Style === 2);
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image
+        <ImageBackground
           source={{uri: `${BASE_URL}${ImageResponse?.Url}`}}
-          style={styles.image}
-        />
+          style={styles.image}>
+          {SupportMultiWishlists && WishlistEnabled && (
+            <FavoriteIcon stroke={colors.tabsColor} />
+          )}
+          {Price?.HasDiscount && DiscountBadge?.Label && (
+            <View style={styles.discountBadgeContainer}>
+              <DiscountIcon />
+              <Text
+                style={styles.discountBadge}
+                variant="xSmallLight"
+                text={DiscountBadge?.Label}
+              />
+            </View>
+          )}
+          {isNewBadge && (
+            <Text
+              style={styles.isNewBadge}
+              variant="xSmallLight"
+              text={isNewBadge?.Label}
+            />
+          )}
+        </ImageBackground>
       </View>
       <View style={styles.productNameContainer}>
         <Text
@@ -40,11 +54,42 @@ const RowProductCard = (props: IProductInterface) => {
           variant="mediumRegular"
           color={colors.tabsColor}
           numberOfLines={2}
-          style={{width: '80%'}}
         />
+        <View style={styles.row}>
+          <Text
+            tx="product-details.category"
+            variant="mediumRegular"
+            color={colors.orange}
+            numberOfLines={2}
+            style={styles.categoryHint}
+          />
+          <Text
+            text={CategoryName}
+            variant="mediumRegular"
+            color={colors.tabsColor}
+            numberOfLines={2}
+          />
+        </View>
       </View>
       <View style={styles.priceContainer}>
-        <Text>asd</Text>
+        <Text
+          text={Price?.RegularPrice}
+          variant="xSmallLight"
+          style={styles.prevPrice}
+          color={colors.grayMainBolder}
+        />
+        <Text
+          text={Price?.Price}
+          variant="mediumBold"
+          color={colors.orange}
+          numberOfLines={1}
+        />
+        <Text
+          text={currency?.Symbol}
+          variant="xSmallRegular"
+          color={colors.tabsColor}
+          style={styles.currency}
+        />
       </View>
     </View>
   );
@@ -57,21 +102,66 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    height: 100,
+    height: 103,
     borderRadius: spacing.medium - 2,
     overflow: 'hidden',
+    backgroundColor: colors.secondaryBackground2,
+    marginTop: spacing.normal - 1,
   },
   image: {
     flex: 1,
+    padding: 5,
   },
   imageContainer: {
     flex: 0.25,
   },
   productNameContainer: {
     flex: 0.5,
+    paddingHorizontal: spacing.normal - 1,
   },
   priceContainer: {
     flex: 0.25,
-    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  currency: {
+    textTransform: 'uppercase',
+  },
+  prevPrice: {
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  categoryHint: {
+    marginHorizontal: spacing.tiny,
+  },
+  discountBadgeContainer: {
+    position: 'absolute',
+    bottom: 5,
+    right: 5,
+    paddingHorizontal: spacing.smaller,
+    paddingVertical: spacing.tiny,
+    borderRadius: 5,
+    overflow: 'hidden',
+    backgroundColor: colors.orangeDark,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  discountBadge: {
+    color: colors.white,
+  },
+  isNewBadge: {
+    backgroundColor: colors.orange,
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    color: colors.white,
+    paddingHorizontal: spacing.smaller,
+    paddingVertical: spacing.tiny,
+    borderRadius: 5,
+    overflow: 'hidden',
   },
 });
