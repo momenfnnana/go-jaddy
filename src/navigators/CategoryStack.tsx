026a3d47fe2ea1db4bf8437c6326c1792691501d
Momen Fnnana <mommen@hemma.sa>
2022-10-12 01:18:50 +0300

    remove unnecessary code from categories stack

--- src/navigators/CategoryStack.tsx
+++ src/navigators/CategoryStack.tsx
@@ -19,56 +19,6 @@ const CategoriesStack = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        // header: ({navigation, ...props}) => {
-        //   const Left = (props.options as any).headerLeft;
-        //   return (
-        //     <View
-        //       style={{
-        //         backgroundColor: colors.primary,
-        //         paddingTop: top + 5,
-        //         paddingBottom: 10,
-        //         paddingHorizontal: spacing.content,
-        //         flexDirection: 'row',
-        //         justifyContent: 'space-between',
-        //         alignItems: 'center',
-        //         height: 100,
-        //         // flex: 1,
-        //       }}>
-        //       {/* <Pressable
-        //       onPress={() => navigation.goBack()}
-        //       disabled={navigation.canGoBack()}
-        //       style={{
-        //         backgroundColor: colors.white + 18,
-        //         padding: 10,
-        //         borderRadius: 5,
-        //         opacity: navigation.canGoBack() ? 1 : 0,
-        //       }}>
-        //       <ArrowIcon />
-        //     </Pressable> */}
-        //       <View style={{width: 150, flexDirection: 'row'}}>
-        //         {Left != undefined && <Left />}
-        //       </View>
-        //       <Text
-        //         tx={props.options.title || 'category.title'}
-        //         color={colors.white}
-        //         variant="mediumRegular"
-        //         center
-        //       />
-        //       <View style={{width: 150, flexDirection: 'row', opacity: 0}}>
-        //         {Left != undefined && <Left />}
-        //       </View>
-        //       {/* <Pressable
-        //       style={{
-        //         backgroundColor: colors.white + 18,
-        //         padding: 10,
-        //         borderRadius: 5,
-        //         opacity: 0,
-        //       }}>
-        //       <ArrowIcon />
-        //     </Pressable> */}
-        //     </View>
-        //   );
-        // },
         headerStyle: {backgroundColor: colors.primary},
         headerTitleStyle: {fontFamily: font.regular, color: colors.white},
       }}>
