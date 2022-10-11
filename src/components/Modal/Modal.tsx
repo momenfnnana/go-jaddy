c902c7c6e8ae736cb3a2c1c60325a23b996cd390
Momen Fnnana <mommen@hemma.sa>
2022-10-12 01:17:38 +0300

    clean up modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,31 +1,18 @@
-import React, {ReactNode, useEffect, useState} from 'react';
+import React, {ReactNode} from 'react';
 import {
-  FlatList,
-  Pressable,
-  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   useWindowDimensions,
   View,
   ViewStyle,
 } from 'react-native';
 import {colors, spacing} from 'theme';
-import Icon from 'react-native-vector-icons/AntDesign';
-import {Button, InputField, Text, Loader} from 'components';
-import {useNavigation} from '@react-navigation/native';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {AuthRoutes} from 'navigators/RoutesTypes';
-import {Formik} from 'formik';
-import * as Yup from 'yup';
-import {useTranslation} from 'react-i18next';
-import {useMutation} from '@tanstack/react-query';
-import {reset_password} from 'services/Auth';
-import {AuthNavigationsType} from 'navigators/NavigationsTypes';
+
+import {Text} from 'components';
 import RNModal from 'react-native-modal';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import {postAddToWishlist, postCreateWishlist} from 'services/Home';
-import {BASE_URL} from 'utils/Axios';
 
 interface IResetPasswordModalProps {
   style?: ViewStyle;
@@ -41,55 +28,58 @@ interface IResetPasswordModalProps {
   title: string;
   description: string;
   children: ReactNode;
+  containerStyle: ViewStyle;
 }
-
 
 const ICON_WIDTH = 30;
 const SIZE = 18;
 
-
 const Modal: React.FC<IResetPasswordModalProps> = ({
-  visibleResetPasswordModal,
   setvisibleResetPasswordModal,
   success,
-  data,
   isVisible,
   onBackdropPress,
-  isLoading,
   forceRefetch,
-  ProductId,
   title,
   description,
   children,
+  containerStyle,
 }) => {
-  const {height, width} = useWindowDimensions();
+  const {width} = useWindowDimensions();
   const {bottom} = useSafeAreaInsets();
 
   return (
     <RNModal isVisible={isVisible} onBackdropPress={onBackdropPress}>
-      <View
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={[
           styles.container,
-          {height: height * 0.5, width, bottom: -bottom, paddingBottom: bottom},
+          containerStyle,
+          {
+            width,
+            bottom: -bottom,
+          },
         ]}>
-        <View style={styles.contentConteiner}>
-          <AntDesign
-            name="close"
-            size={spacing.xxLarge}
-            color={colors.tabsColor + 40}
-            style={styles.closeBtn}
-            onPress={onBackdropPress}
-          />
-          <Text tx={title} variant="mediumBold" color={colors.tabsColor} />
-          <Text
-            tx={description}
-            variant="smallRegular"
-            color={colors.tabsColor + 70}
-            style={styles.titleHint}
-          />
+        <View style={{paddingBottom: bottom}}>
+          <View style={styles.contentConteiner}>
+            <AntDesign
+              name="close"
+              size={spacing.xxLarge}
+              color={colors.tabsColor + 40}
+              style={styles.closeBtn}
+              onPress={onBackdropPress}
+            />
+            <Text tx={title} variant="mediumBold" color={colors.tabsColor} />
+            <Text
+              tx={description}
+              variant="smallRegular"
+              color={colors.tabsColor + 70}
+              style={styles.titleHint}
+            />
+          </View>
+          {children}
         </View>
-        {children}
-      </View>
+      </KeyboardAvoidingView>
     </RNModal>
   );
 };
