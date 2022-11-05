529020d8cbc15e9db59f128d4bca425d8ce07aa2
Momen Fnnana <mommen@hemma.sa>
2022-11-05 20:06:04 +0200

    hide error message for error 401

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,7 +1,8 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import {showErrorMessage} from 'helpers';
 import * as RootNavigation from 'navigators/RootNavigation';
-// import Snackbar from 'react-native-snackbar';
+import Snackbar from 'react-native-snackbar';
 import {colors} from 'theme';
 
 export const setAxiosLanguage = (languageId: string) => {
@@ -17,19 +18,25 @@ export const setAxiosCurrencyId = (CurrencyId: number) => {
 axios.interceptors.response.use(
   response => {
     if (response.status == 200 && response.data.Message) {
-      // Snackbar.show({
-      //   text: response.data.Message,
-      //   duration: Snackbar.LENGTH_SHORT,
-      //   backgroundColor: colors.success,
-      // });
+      Snackbar.show({
+        text: response.data.Message,
+        duration: Snackbar.LENGTH_SHORT,
+        backgroundColor: colors.success,
+      });
     }
     return response;
   },
   error => {
     if (error?.response?.status === 401) {
+      AsyncStorage.removeItem('accessToken');
       // return RootNavigation.navigate('AuthFlow', {screen: 'Login'} as any);
     }
-    showErrorMessage(error?.response?.data?.Message);
+    if (
+      !!error?.response?.data?.Message.length &&
+      error?.response?.status !== 401
+    ) {
+      showErrorMessage(error?.response?.data?.Message);
+    }
     throw error;
   },
 );
