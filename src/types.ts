a73e8e7fa617d6f355d6311900289642b00fad1b
Momen Fnnana <mommen@hemma.sa>
2022-11-11 23:05:56 +0200

    export new interface type for addresses

--- src/types.ts
+++ src/types.ts
@@ -11,5 +11,21 @@ interface ImageData {
   Title: string;
   Url: string;
 }
-
-export type {ICurrency, ImageData};
+interface IAddress {
+  Id: number;
+  Company: string;
+  FirstName: string;
+  LastName: string;
+  Address1: string;
+  Address2: string;
+  CountryId: number;
+  CountryName: string;
+  City: string;
+  Email: string;
+  PhoneNumber: string;
+  FaxNumber: string;
+  StateId: string;
+  IsDefault: boolean;
+}
+export const CART = 'CART';
+export type {ICurrency, ImageData, IAddress};
