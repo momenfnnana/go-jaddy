574e073fa8d917fa18446cccc873939ae93076f4
Momen Fnnana <mommen@hemma.sa>
2022-11-16 00:30:48 +0200

    change components names

--- src/screens/main/checkout/components/index.ts
+++ src/screens/main/checkout/components/index.ts
@@ -1,5 +1,9 @@
 export {default as CheckoutHeader} from './CheckoutHeader';
-export {default as CheckoutFirstStep} from './CheckoutFirstStep';
-export {default as CheckoutSecondStep} from './CheckoutSecondStep';
-export {default as CheckoutThirdStep} from './CheckoutThirdStep';
-export {default as CheckoutFourthStep} from './CheckoutFourthStep';
+export {default as CheckoutStepOne} from './CheckoutStepOne';
+export {default as CheckoutStepTwo} from './CheckoutStepTwo';
+export {default as CheckoutStepThree} from './CheckoutStepThree';
+export {default as CheckoutStepFour} from './CheckoutStepFour';
+
+export interface ICheckoutStep {
+  setActiveStep?: any;
+}
