ca3d3a316941c838da5626b1cab9393ba62915bc
Momen Fnnana <mommen@hemma.sa>
2022-11-20 20:24:31 +0200

    make IAddress types optional

--- src/types.ts
+++ src/types.ts
@@ -12,20 +12,20 @@ interface ImageData {
   Url: string;
 }
 interface IAddress {
-  Id: number;
-  Company: string;
-  FirstName: string;
-  LastName: string;
-  Address1: string;
-  Address2: string;
-  CountryId: number;
-  CountryName: string;
-  City: string;
-  Email: string;
-  PhoneNumber: string;
-  FaxNumber: string;
-  StateId: string;
-  IsDefault: boolean;
+  Id?: number;
+  Company?: string;
+  FirstName?: string;
+  LastName?: string;
+  Address1?: string;
+  Address2?: string;
+  CountryId?: number;
+  CountryName?: string;
+  City?: string;
+  Email?: string;
+  PhoneNumber?: string;
+  FaxNumber?: string;
+  StateId?: string;
+  IsDefault?: boolean;
 }
 export const CART = 'CART';
 export type {ICurrency, ImageData, IAddress};
