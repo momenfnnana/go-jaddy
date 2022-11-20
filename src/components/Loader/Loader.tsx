1f4e7b2885f41ceeff1cc9a21e2b8acf3578ca06
Momen Fnnana <mommen@hemma.sa>
2022-11-20 20:28:03 +0200

    refactoring on loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {
   View,
   ActivityIndicator,
@@ -10,19 +10,38 @@ import {colors} from 'theme';
 interface ILoader extends ActivityIndicatorProps {
   containerStyle?: ViewStyle | ViewStyle[];
   isPageLoading?: boolean;
+  variant?: 'screenContent';
 }
 const Loader = ({
   size,
   containerStyle,
   isPageLoading = false,
+  variant,
   ...rest
 }: ILoader) => {
+  const customStyleContainer: ViewStyle | ViewStyle[] | undefined =
+    useMemo(() => {
+      if (variant === 'screenContent') {
+        return {
+          backgroundColor: colors.black + 40,
+          justifyContent: 'center',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+          zIndex: 1,
+        };
+      } else {
+        return containerStyle;
+      }
+    }, [variant]);
   return (
     <View
       style={
         isPageLoading
           ? {flex: 1, justifyContent: 'center', alignItems: 'center'}
-          : containerStyle
+          : customStyleContainer
       }>
       <ActivityIndicator
         color={colors.secondary}
